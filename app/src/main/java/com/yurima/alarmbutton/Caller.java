86a1e5e1e6ecd8f3a7da26bf22a364419d2a49c6
Yury <31337@mail.ru>
2019-01-22 18:10:13 +0400

    added the smsAlarm method

--- app/src/main/java/com/yurima/alarmbutton/Caller.java
+++ app/src/main/java/com/yurima/alarmbutton/Caller.java
@@ -6,24 +6,41 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
+import android.telephony.SmsManager;
+import android.widget.Toast;
 
 public class Caller {
 
     private Context context;
+    final private String phoneNo = "+79279826090";
 
     public Caller (Context context) {
         this.context = context;
     }
 
-    public void PhoneAlarm (){
+    public void phoneAlarm(){
         Intent phoneIntent = new Intent(Intent.ACTION_CALL);
-        phoneIntent.setData(Uri.parse("tel:+79279826090"));
+        phoneIntent.setData(Uri.parse("tel:" + phoneNo));
 
         if (ActivityCompat.checkSelfPermission(context,
                 Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             return;
+            //TODO make mistake notification
         }
         context.startActivity(phoneIntent);
+    }
 
+    public void smsAlarm () {
+        try {
+            String msg = "Alarm!Alarm!";
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
+            Toast.makeText(context.getApplicationContext(), "Message Sent",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(context.getApplicationContext(),ex.getMessage(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/yurima/alarmbutton/MainActivity.java
+++ app/src/main/java/com/yurima/alarmbutton/MainActivity.java
@@ -1,17 +1,12 @@
 package com.yurima.alarmbutton;
 
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatImageButton;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     @OnClick (R.id.alarm_button)
     public void onClickAlarmButton (ImageButton ib) {
         //Toast.makeText(this, "Alarm! Help! Sos!", Toast.LENGTH_SHORT).show();
-        caller.PhoneAlarm();
+        //caller.phoneAlarm();
+        caller.smsAlarm();
     }
 
     @Override
