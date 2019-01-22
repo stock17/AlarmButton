9e9507028a0d8c8e1519d9f8813fbf609aa16074
Yury <31337@mail.ru>
2019-01-22 17:39:38 +0400

    added a listener for the button

--- app/src/main/java/com/yurima/alarmbutton/MainActivity.java
+++ app/src/main/java/com/yurima/alarmbutton/MainActivity.java
@@ -5,20 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageButton;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
 
 
     @BindView(R.id.alarm_button) ImageButton alarmButton;
+    @OnClick (R.id.alarm_button)
+    public void onClickAlarmButton (ImageButton ib) {
+        Toast.makeText(this, "Alarm! Help! Sos!", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
 
 
     }
