c2f901bf00e1a76d6ec662d02c391dd0e7ef5203
Yury <31337@mail.ru>
2019-02-11 16:56:08 +0400

    performed onReceive method

--- conductor/src/main/java/com/yurima/conductor/SMSReceiver.java
+++ conductor/src/main/java/com/yurima/conductor/SMSReceiver.java
@@ -6,10 +6,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
-
-/**
- * Created by Yury on 11.02.2019.
- */
+import android.widget.Toast;
 
 public class SMSReceiver extends BroadcastReceiver {
 
@@ -18,16 +15,35 @@ public class SMSReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        smsMessages = getSmsMessages(intent);
+        if (smsMessages != null){
+            for (SmsMessage msg : smsMessages){
+                showMessage(context, msg);
+            }
+        }
+    }
+
+    private SmsMessage[] getSmsMessages(Intent intent){
         Bundle extras = intent.getExtras();
         if (extras != null) {
             Object[] pdus = (Object[]) extras.get("pdus");
             if (pdus != null) {
-                smsMessages = new SmsMessage[pdus.length];
+                SmsMessage[] messages = new SmsMessage[pdus.length];
                 for (int i = 0; i < pdus.length; i++) {
-                    smsMessages[i] = getIncomingMessage(pdus[i], extras);
+                    messages[i] = getIncomingMessage(pdus[i], extras);
                 }
+                return messages;
             }
         }
+        return null;
+    }
+
+    private void showMessage(Context context, SmsMessage msg){
+
+        String address = msg.getOriginatingAddress();
+        String text = msg.getMessageBody();
+        Toast.makeText(context, "from: " + address + "\n" +
+                        "text: " + text, Toast.LENGTH_LONG ).show();
     }
 
     private SmsMessage getIncomingMessage(Object aObject, Bundle bundle) {
