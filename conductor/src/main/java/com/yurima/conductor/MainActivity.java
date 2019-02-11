7a5eaa0cd48634e7b511baa718e6bbd8f5b73aed
Yury <31337@mail.ru>
2019-02-11 18:20:21 +0400

    performed service starting, the receiver doesn't work

--- conductor/src/main/java/com/yurima/conductor/MainActivity.java
+++ conductor/src/main/java/com/yurima/conductor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yurima.conductor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,13 +11,16 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private ConductorService mConductorService;
 
     @BindView(R.id.button_start)
     Button startButton;
     @OnClick(R.id.button_start)
     void onClickStartButton(){
-        //TODO
+        mConductorService = new ConductorService();
+        Intent serviceIntent = new Intent(getApplicationContext(), ConductorService.class);
+        startService(serviceIntent);
+
     }
 
     @Override
--- conductor/src/main/java/com/yurima/conductor/SmsReceiver.java
+++ conductor/src/main/java/com/yurima/conductor/SmsReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver {
@@ -44,6 +45,7 @@ public class SmsReceiver extends BroadcastReceiver {
         String text = msg.getMessageBody();
         Toast.makeText(context, "from: " + address + "\n" +
                         "text: " + text, Toast.LENGTH_LONG ).show();
+        Log.i("Info", "Received!!!" );
     }
 
     private SmsMessage getIncomingMessage(Object aObject, Bundle bundle) {
